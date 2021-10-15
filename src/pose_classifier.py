418cf5532b12f4d0923dc788b71aa74f50f77706
kkin1995 <karan@akinariwala.com>
2021-10-15 23:04:57 +0530

    Split training set into train and validation sets with a 70-30 split. Used the test set to evaluate the trained model. Plotted training loss and accuracy and validation loss and accuracy.

--- src/pose_classifier.py
+++ src/pose_classifier.py
@@ -23,42 +23,51 @@ for c in classes:
     train_files = np.append(train_files, train)
     test_files = np.append(test_files, test)
     no_of_train_files += len(train)
-    no_of_test_files += len(test)
-    
-files = np.append(train_files, test_files)
-no_of_files = no_of_train_files + no_of_test_files
+    no_of_test_files += len(test)  
 
 one_hot_classes = dict(pd.get_dummies(classes))
 
-batch_size = len(files)
+batch_size = len(train_files)
+test_batch_size = len(test_files)
 input_dim = 34 # Number of Keypoints / Input Dimension
 no_of_classes = len(classes) # Number of Classes / Output Dimension
 data_shape = (batch_size, input_dim)
+test_data_shape = (test_batch_size, input_dim)
 labels_shape = (batch_size, no_of_classes)
+test_labels_shape = (test_batch_size, no_of_classes)
 
 data = np.zeros(data_shape)
+test_data = np.zeros(test_data_shape)
 labels = np.zeros(labels_shape)
+test_labels = np.zeros(test_labels_shape)
 
 for i, c in enumerate(classes):
-    for j, f in enumerate(files):
+    for j, f in enumerate(train_files):
         class_of_f = f.split("/")[-2]
         if class_of_f == c:
             data_example = np.load(f)
             data[j, :] = data_example.ravel()
             labels[j, :] = one_hot_classes[c].values
 
+for i, c in enumerate(classes):
+    for j, f in enumerate(test_files):
+        class_of_f = f.split("/")[-2]
+        if class_of_f == c:
+            test_data_example = np.load(f)
+            test_data[j, :] = test_data_example.ravel()
+            test_labels[j, :] = one_hot_classes[c].values
+
 print("Shape of Data = {}".format(data.shape))
 print("Shape of Labels = {}".format(labels.shape))
+print("Shape of Test Data = {}".format(test_data.shape))
+print("Shape of Test Labels = {}".format(test_labels.shape))
 
-X_train, X_test_val, y_train, y_test_val = train_test_split(data, labels, test_size=0.3, random_state=42)
-X_val, X_test, y_val, y_test = train_test_split(X_test_val, y_test_val, test_size = 0.4, random_state = 42)
+X_train, X_val, y_train, y_val = train_test_split(data, labels, test_size=0.3, random_state=42)
 
 print("Shape of Training Data = {}".format(X_train.shape))
 print("Shape of Training Labels = {}".format(y_train.shape))
 print("Shape of Validation Data = {}".format(X_val.shape))
 print("Shape of Validation Labels = {}".format(y_val.shape))
-print("Shape of Test Data = {}".format(X_test.shape))
-print("Shape of Test Labels = {}".format(y_test.shape))
 
 model = keras.Sequential([
     Input(shape = (34,)),
@@ -69,20 +78,26 @@ model = keras.Sequential([
 opt = keras.optimizers.Adam(learning_rate = LEARNING_RATE)
 model.compile(optimizer=opt, loss="categorical_crossentropy", metrics = "accuracy")
 
-history = model.fit(x = X_train, y = y_train, epochs = EPOCHS)
+history = model.fit(x = X_train, y = y_train, validation_data = (X_val, y_val), epochs = EPOCHS)
 
-evaluation_metrics = model.evaluate(X_val, y_val)
+test_metrics = model.evaluate(test_data, test_labels)
 
-print("Evaluation Loss = {}".format(evaluation_metrics[0]))
-print("Evaluation Accuracy = {}".format(evaluation_metrics[1]))
+print("Test Loss = {}".format(test_metrics[0]))
+print("Test Accuracy = {}".format(test_metrics[1]))
 
-fig, axs = plt.subplots(1, 2)
+fig, axs = plt.subplots(2, 2)
 plt.suptitle("Training Metrics")
-axs[0].plot(history.history["accuracy"])
-axs[0].set_title("Training Accuracy")
-axs[0].set_xlabel("Epochs")
-axs[1].plot(history.history["loss"])
-axs[1].set_title("Training Loss")
-axs[1].set_xlabel("Epochs")
+axs[0, 0].plot(history.history["accuracy"])
+axs[0, 0].set_title("Training Accuracy")
+axs[0, 0].set_xlabel("Epochs")
+axs[0, 1].plot(history.history["loss"])
+axs[0, 1].set_title("Training Loss")
+axs[0, 1].set_xlabel("Epochs")
+axs[1, 0].plot(history.history["val_accuracy"])
+axs[1, 0].set_title("Validation Accuracy")
+axs[1, 0].set_xlabel("Epochs")
+axs[1, 1].plot(history.history["val_loss"])
+axs[1, 1].set_title("Validation Loss")
+axs[1, 1].set_xlabel("Epochs")
 
 plt.show()
